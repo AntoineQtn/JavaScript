eeefc11e7378e2d2bff72b435232b42daaf6bb1e
Antoine <antoine.quintin@le-campus-numerique.fr>
2025-04-24 16:47:59 +0200

    Exo2 : Conditions JavaScript

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,18 @@
 // you can write js here
 console.log('exo-2');
+
+const myDate = new Date();
+console.log(myDate);
+
+let affichage_semaine = myDate.getDay();
+//console.log("Nous sommes un jour de semaine.");
+
+let affichage_weekend = 
+//console.log("nous sommes un jour de weekend.");
+
+/*if(myDate!=affichage_weekend){
+    console.log(affichage_semaine);
+}
+else{
+    console.log(aff)
+}*/
