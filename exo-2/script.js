b99bb179ed8c8631e9681676507f722647ed5eb3
Antoine <antoine.quintin@le-campus-numerique.fr>
2025-04-25 10:44:47 +0200

    Modif exo2

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,18 +1,31 @@
 // you can write js here
 console.log('exo-2');
 
+const dayWeek = "On est en semaine"
+const dayWeekend = "c'est le week-end"
+
 const myDate = new Date();
 console.log(myDate);
 
-let affichage_semaine = myDate.getDay();
-//console.log("Nous sommes un jour de semaine.");
+let day = myDate.getDay();
+console.log(day);
 
-let affichage_weekend = 
-//console.log("nous sommes un jour de weekend.");
+const hour = myDate.getHours();
+console.log(hour);
 
-/*if(myDate!=affichage_weekend){
-    console.log(affichage_semaine);
+const isTesting = true;
+
+if(isTesting){
+    
 }
-else{
-    console.log(aff)
-}*/
+
+if (day != 6 && day != 0) {
+    console.log(dayWeek)
+}
+if (day == 5 && hour > 17) {
+    console.log(dayWeekend)
+}
+if (day == 1 && hour < 9) {
+    console.log(dayWeekend)
+}
+else { console.log(dayWeekend) }
