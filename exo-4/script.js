231c6351f5af4b9bcf633e9613e46a310475d95e
Antoine Quintin <antoine.quintin@le-campus-numerique.fr>
2025-04-26 13:31:59 +0200

    début exo 4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,18 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+console.log(secretMessage);
+
+//méthode enlevant le dernier élément du tableau
+
+//méthode ajoutant les mots "to" et "program" comme des éléments distincts à la fin du tableau
+
+//change le mot "easily" par "right" en accédant au bon index du tableau
+
+//méthode qui supprime le premier élément du tableau
+
+//méthode ajoutant la chaîne "Programing" en début de tableau
+
+//méthode remplaçant les cinq mots "get" "right" "the" "first" "time" par le mot "know"
+
+//utilisation de la méthode join pour afficher un message secret dans la console.
