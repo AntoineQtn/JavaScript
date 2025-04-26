8d02559ae50a62bbb0a5444c980c1b636c9b3fe5
Antoine Quintin <antoine.quintin@le-campus-numerique.fr>
2025-04-26 13:20:49 +0200

    Exo 3 presque terminé

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,6 +1,56 @@
 // you can write js here
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+
+var playerInput = prompt("Your choice ( rock, paper, scissors) ?");
+
+function getPlayerChoice(playerInput) {
+  playerInput = playerInput.toLowerCase();
+
+  if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") {
+    return playerInput;
+  }
+  else { return null; }
 }
 
-console.log(getRandomInt(3));
+var randomInt = Math.floor(Math.random() * 3);
+
+function getComputerChoice() {
+
+  if (randomInt === 0) {
+    return "rock";
+  }
+  else if (randomInt === 1) {
+    return "paper";
+  }
+  else {
+    return "scissors";
+  }
+}
+
+function findWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "Tied";
+  }
+  else if (
+    (playerChoice === rock && computerChoice === scissors) ||
+    (playerChoice === paper && computerChoice === rock) ||
+    (playerChoice === scissors && computerChoice === paper)
+  ) {
+    return "won";
+  } else {
+    return "lost";
+  }
+}
+
+function playGame() {
+  var uChoice = getPlayerChoice(playerInput);
+  console.log("Your choice " + uChoice);
+
+  var computerChoice = getComputerChoice();
+  console.log("Computer's choice " + computerChoice);
+
+  var result = findWinner(uChoice, computerChoice);
+  console.log("Result: " + result);
+  alert(result);
+}
+
+playGame();
