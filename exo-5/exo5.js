8e02bfc793d6d5f08d0aca59e54013da8c300300
Antoine Quintin <antoine.quintin@le-campus-numerique.fr>
2025-04-26 19:51:15 +0200

    exercice 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,17 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = prompt();
+let vowels = ["a", "e", "i", "o", "u", "y"];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    let lettre = input[i];
+    console.log(lettre);
+    if (vowels.indexOf(lettre.toLowerCase()) !== -1) {
+        resultArray.push(lettre);
+    }
+        let traduction = resultArray.join('').toUpperCase();
+console.log(traduction);
+}
