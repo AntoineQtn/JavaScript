a41c256ba83db419ebdf005dfb23e96ae2354132
Antoine Quintin <antoine.quintin@le-campus-numerique.fr>
2025-04-27 19:33:29 +0200

    Exo 6, début

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -11,7 +11,29 @@ var joeInfo = {
 // ! Ne faites pas de modification au dessus de cette ligne !
 
 // 1. Afficher le nombre de voitures de Joe
-
+console.log(joeInfo.cars);
 // 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain.
-
+console.log(joeInfo.bathrooms = 1);
 // 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
+console.log(joeInfo.garage = 1);
+
+var team = {
+    players: [{
+        firstName: 'Pablo',
+        lastName: 'Sanchez',
+        age: 11,
+    }],
+    games: [{
+        opponent: "Broncos",
+        teamPoints: 42,
+        opponentPoints: 27,
+    }],
+    addPlayer(){
+        firstName
+        lastName
+        age
+        team.players.push();
+    }
+}
+
+console.log(team.addPlayer());
